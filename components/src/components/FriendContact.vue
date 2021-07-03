b3241a670b8e99c8ec6e70264922b7c14f8be831
hasanAbdullah9825 <hasan.techtomorrow@gmail.com>
2021-07-04 01:30:30 +0600

    Prop behavior and changing props

--- components/src/components/FriendContact.vue
+++ components/src/components/FriendContact.vue
@@ -1,45 +1,52 @@
 <template>
-    <ul>
+  <ul>
+    <li>
+      <h2>{{ name }} {{ friendIsFavorite === "1" ? "(favorite)" : "" }}</h2>
+      <button @click="toggleDetails">ShowDetails</button>
+      <button @click="toggleFavorite">toggleFavorite</button>
+      <ul v-if="detailsAreVisible">
         <li>
-        <h2>{{name}}</h2>
-        <button @click="toggleDetails" >ShowDetails</button>
-        <ul v-if ="detailsAreVisible">
-            <li>
-                <strong>phone</strong>
-                {{phoneNumber}}
-            </li>
-            <li>
-                <strong>Email</strong>
-                {{emailAddress}}
-
-            </li>
-            
-        </ul>
+          <strong>phone</strong>
+          {{ phoneNumber }}
+        </li>
+        <li>
+          <strong>Email</strong>
+          {{ emailAddress }}
+        </li>
+      </ul>
     </li>
-    </ul>
+  </ul>
 </template>
 <script>
 export default {
-    props:["name","emailAddress","phoneNumber"],
-    data(){
- return{
-     detailsAreVisible: false,
+  props: ["name", "emailAddress", "phoneNumber", "isFavorite"],
+  data() {
+    return {
+      detailsAreVisible: false,
       friend: {
         id: "manuel",
         name: "Manuel Lorenz",
         phone: "0123 45678 90",
-        email: "manuel@localhost.com"
- }
- }
- },
- methods:{
-     toggleDetails(){
-         this.detailsAreVisible=!this.detailsAreVisible;
-         
-     }
- }
-}
+        email: "manuel@localhost.com",
+      },
+      friendIsFavorite:this.isFavorite
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+
+        if(this.friendIsFavorite==='1'){
+            this.friendIsFavorite="0";
+        }
+        else{
+            this.friendIsFavorite="1";
+        }
+    },
+  },
+};
 </script>
 <style >
-
 </style>
