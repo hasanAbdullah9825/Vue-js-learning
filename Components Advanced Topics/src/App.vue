2584a77754ee29468b7070ccaddf19f218e55d47
hasanAbdullah9825 <hasan.techtomorrow@gmail.com>
2021-07-14 12:03:53 +0600

    Global VS Local component

---
+++ Components Advanced Topics/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
+</template>
+
+<script>
+import BadgeList from "./components/BadgeList.vue";
+import TheHeader from "./components/TheHeader.vue";
+import UserInfo from "./components/UserInfo.vue";
+export default {
+  components: { TheHeader, BadgeList, UserInfo },
+  data() {
+    return {
+      activeUser: {
+        name: "Abdullah Al Hasan",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+  html {
+    font-family: sans-serif;
+  }
+
+  body {
+    margin: 0;
+  }
+</style>
